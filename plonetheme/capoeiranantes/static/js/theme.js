7136dbf4cd7dabfdc31eb1c0dad467dcceb225df
JeanMichel FRANCOIS <toutpt@gmail.com>
2011-12-12 19:01:05 +0100

    initial commit

---
+++ plonetheme/capoeiranantes/static/js/theme.js
@@ -0,0 +1,5 @@
+
+function initializeplonethemecapoeiranantes(){
+
+}
+jQuery(initializeplonethemecapoeiranantes);
--- setup.py
+++ setup.py
@@ -5,19 +5,22 @@ version = '1.0'
 
 setup(name='plonetheme.capoeiranantes',
       version=version,
-      description="",
-      long_description=open("README.txt").read() + "\n" +
+      description="An installable diazo theme for Plone 4",
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
-        "Framework :: Plone",
         "Programming Language :: Python",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
         ],
       keywords='',
       author='',
       author_email='',
-      url='https://github.com/toutpt/plonetheme.capoeiranantes',
+      url='http://svn.plone.org/svn/collective/',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['plonetheme'],
