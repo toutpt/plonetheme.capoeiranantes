4ca2f8cdf2ede4a7e536ec6413cbc4d0aaf9c163
JeanMichel FRANCOIS <toutpt@gmail.com>
2011-12-27 14:36:21 +0100

    update

--- README.rst
+++ README.rst
@@ -3,3 +3,11 @@ Introduction
 
 The for Plone for the website capoeira-nantes.fr
 
+Colors
+======
+
+greens:
+
+#DAECE2
+#369F63
+#5EB581
--- plonetheme/capoeiranantes/static/js/theme.js
+++ plonetheme/capoeiranantes/static/js/theme.js
@@ -1,5 +1,17 @@
-
+function updateSearchBox(){
+	$('#livesearch0 .searchSection').remove();
+}
+function equalHeight(group) {
+	var tallest = 0;
+	group.each(function() {
+		var thisHeight = $(this).height();
+		if(thisHeight > tallest) {
+			tallest = thisHeight;
+		}
+	});
+	group.height(tallest);
+}
 function initializeplonethemecapoeiranantes(){
-
+    updateSearchBox();
 }
 jQuery(initializeplonethemecapoeiranantes);
