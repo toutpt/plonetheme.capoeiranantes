5e4b0a33b11fa780670417837ef938848301f343
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-05-01 18:43:43 +0200

    override galleria_view template to use large scale

--- plonetheme/capoeiranantes/browser/interfaces.py
+++ plonetheme/capoeiranantes/browser/interfaces.py
@@ -1,6 +1,7 @@
 from zope import interface
 from plone.app.discussion.interfaces import IDiscussionLayer
+from collective.galleria.interfaces import IGalleriaLayer
 
 
-class IThemeSpecific(interface.Interface, IDiscussionLayer):
+class IThemeSpecific(interface.Interface, IDiscussionLayer, IGalleriaLayer):
     """Marker interface that defines a Zope 3 browser layer."""
